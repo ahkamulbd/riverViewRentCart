3df74cfc552d9df6d8328b76cae16cc536044e1f
ahkamul <ahkamulbd@gmail.com>
2025-02-02 00:11:40 +0600

    js file linked and input field id named

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let bristiChoyaField = document.getElementById('bristiChoya-field');
+let bristiChoyaFieldText = bristiChoyaField.innerText;
+
+console.log(bristiChoyaFieldText);
+
+
+let meghViewField = document.getElementById('meghView-filed');
+let meghViewFieldText = meghViewField.innerText;
+
+let ghomGhorField = document.getElementById('ghomGhor-filed');
+let ghomGhorFieldText = ghomGhorField.innerText;
+
+let tentField = document.getElementById('tent-field');
+let tentFieldText = tentField.innerText;
