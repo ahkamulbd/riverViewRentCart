961794db33368ab7990770a55c6482852e3bcc8a
ahkamul <ahkamulbd@gmail.com>
2025-02-02 12:24:30 +0600

    Javascript codes written to count total rent

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
-let bristiChoyaField = document.getElementById('bristiChoya-field');
-let bristiChoyaFieldText = bristiChoyaField.innerText;
+document.getElementById('click-btn').addEventListener('click', () =>{
+    let bristiChoyaField = document.getElementById('bristiChoya-field');
+    let bristiChoyaFieldText = bristiChoyaField.value;
+    let bristiChoyaRent = parseFloat(bristiChoyaFieldText);
 
-console.log(bristiChoyaFieldText);
+    //  console.log(typeof(bristiChoyaRent));
+
+    let meghViewField = document.getElementById('meghView-field');
+    let meghViewFieldText = meghViewField.value;
+    let meghViewRent = parseFloat(meghViewFieldText);
+
+    let ghomGhorField = document.getElementById('ghomGhor-filed');
+    let ghomGhorFieldText = ghomGhorField.value;
+    let ghomGhorRent = parseFloat(ghomGhorFieldText);
+
+    let tentField = document.getElementById('tent-field');
+    let tentFieldText = tentField.value;
+    let tentRent = parseFloat(tentFieldText);
+
+    let totalRent = bristiChoyaRent + meghViewRent + ghomGhorRent + tentRent; 
+    let totalRentFixed = totalRent.toFixed(2);
+
+    let showTotalRent = document.getElementById('show-total-rent');
+    showTotalRent.innerText = totalRentFixed;
+})
 
 
-let meghViewField = document.getElementById('meghView-filed');
-let meghViewFieldText = meghViewField.innerText;
 
-let ghomGhorField = document.getElementById('ghomGhor-filed');
-let ghomGhorFieldText = ghomGhorField.innerText;
-
-let tentField = document.getElementById('tent-field');
-let tentFieldText = tentField.innerText;
